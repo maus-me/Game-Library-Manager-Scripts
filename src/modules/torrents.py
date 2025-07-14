12f5f310ab58395823c9c73daecf3f182dbf20c8
Maus <107959430+maus-me@users.noreply.github.com>
2025-07-13 20:11:29 -0500

    forgot to readd

--- src/modules/torrents.py
+++ src/modules/torrents.py
@@ -134,7 +134,7 @@ def get_gog_all_games():
     response = requests.get(url)
 
     # Ensure the cache directory exists
-    filename = 'cache/gog_recent_torrents.json'
+    filename = 'cache/gog_all_games.json'
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
     if response.status_code == 200:
